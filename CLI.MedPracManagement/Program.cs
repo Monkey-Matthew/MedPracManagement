79b2eb53548c94851ac894e944cb0025b7fa5176
Matthew-Gonzales <monkeymatthew0217@gmail.com>
2025-09-07 22:20:08 -0400

    Started with adding physician with id that can be searched up. Input handling logic is still work in progress as well as the read function.

--- CLI.MedPracManagement/Program.cs
+++ CLI.MedPracManagement/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CLI.MedPracManagement
 {
@@ -7,6 +9,7 @@ namespace CLI.MedPracManagement
         static void Main(string[] args)
         {
             List<Patient> patients = new List<Patient>(); //Creats a list to store patients
+            List<Physician> physicians = new List<Physician>();
 
             while (true) //Repeats itself until user chooses option 9
             {
@@ -16,6 +19,7 @@ namespace CLI.MedPracManagement
                 Console.WriteLine("Option 2: Create a physician");
                 Console.WriteLine("Option 3: Create an appointment");
                 Console.WriteLine("Option 4: Review a patient's information");
+                Console.WriteLine("Option 5: Review a physician's information");
                 Console.WriteLine("Option 9: Quit Program");
                 Console.WriteLine("-------------------------------------------");
 
@@ -37,13 +41,18 @@ namespace CLI.MedPracManagement
                         patients.Add(new Patient()); 
                         break;
                     case 2:
-                        Console.WriteLine("Creating a physician....");
+                        physicians.Add(new Physician());
                         break;
                     case 3:
                         Console.WriteLine("Creating an appointment....");
                         break;
                     case 4: //Prints out the user's information
                         ReviewPatient(patients);
+                        Console.WriteLine();
+                        break;
+                    case 5: //Prints out the physician's information
+                        ReviewPhysician(physicians);
+                        break;
                         Console.WriteLine();
                         break;
                     case 9: //Quits the program
@@ -68,6 +77,26 @@ namespace CLI.MedPracManagement
 
                 if (found != null) //If patient was found then it prints the patient info
                     found.PrintInfo();
+                else
+                    Console.WriteLine($"Patient with id {id} was not found.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid ID. Please enter a number!");
+            }
+        }
+
+        public static void ReviewPhysician(List<Physician> physicians)
+        {
+            Console.Write("Enter physician ID: "); //Prompts the user for the patient id first
+            string? inputId = Console.ReadLine();
+
+            if (int.TryParse(inputId, out int id)) //Tries to convert string to an int if successful it stores the int value in id
+            {
+                Physician? found = physicians.FirstOrDefault(p => p.GetId() == id); //Uses a LINQ method to search through the list and looks for the first patient to match the inputted id
+
+                if (found != null) //If patient was found then it prints the patient info
+                    found.ReadPhysician();
                 else
                     Console.WriteLine($"Patient with id {id} was not found.");
             }
